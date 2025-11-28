29d1e4655213fa66eb85ffcbba0fa392891f7b06
Mohammad Anis Kamal <Mohammad.Kamal@k202domain.csn.com>
2025-11-28 10:08:11 -0330

    updated the get_deck_list funtion

--- blackjack.py
+++ blackjack.py
@@ -16,18 +16,18 @@ def write_money_file(current_money):
         writer.writerow(["Money", new_balance])
 
 #prototype idea of dealer's show card for the time being
-def get_deck_list():
-    suit= ["Hearts", "Diamonds", "Clubs", "Spades"]
-    rank=["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-    point_value=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+#switching the suit, rank and point_value arg to main
+def get_deck_list(suit, rank, point_value):
+
     """suit_choice=random.choice(suit)
     rank_choice=random.choice(rank)
     print(f"{rank_choice} of {suit_choice}")"""
     deck=[]
     for suits in suit:
-        for ranks in rank:
-            point=point_value
-            deck.append([ranks, suits, point])
+        for i, ranks in enumerate(rank):
+            point=point_value[i]
+            card=[suits, ranks, point]
+            deck.append(card)
         random.shuffle(deck)
 
 
@@ -39,7 +39,10 @@ def get_deck_list():
 
 
 def main():
-    get_deck_list()
+    suit= ["Hearts", "Diamonds", "Clubs", "Spades"]
+    rank=["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    point_value=["2", "3", "4", "5", "6", "7", "8", "9", "10","10","10", "10", "11"]
+    get_deck_list(suit,rank,point_value)
 
 if __name__=="__main__":
     main()
