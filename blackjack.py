e467f5e52a432e4eefc522ab7a31107e05159343
Mkamal-45 <mohammadaniskamal@gmail.com>
2025-11-27 21:49:53 -0330

    removed testing code for csv, added and tested the get_deck_list func. point has yet to be tested

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,5 @@
 import csv
-
+import random
 
 def read_money_file():
     with open("money.csv", newline="") as file:
@@ -15,12 +15,22 @@ def write_money_file(current_money):
         new_balance= round(current_money, 2)
         writer.writerow(["Money", new_balance])
 
+#prototype idea of dealer's show card for the time being
+def get_deck_list():
+    suit= ["Hearts", "Diamonds", "Clubs", "Spades"]
+    rank=["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    point=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+    choice=random.choice(suit)
+    choice_2=random.choice(rank)
+    print(f"{choice_2} of {choice}")
+
+
+
+
 
 
 def main():
-    read_money_file()
-    user_input=int(input("Enter amount: "))
-    write_money_file(user_input)
+    get_deck_lis()
 
 if __name__=="__main__":
     main()
