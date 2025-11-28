c82d265f03a69b55f1d03f072d828356cbfab985
Mkamal-45 <mohammadaniskamal@gmail.com>
2025-11-28 17:49:13 -0330

    created a seperate module as instructed for the read_write funcs and also changed the func name for creating the deck to an approprate one

--- blackjack.py
+++ blackjack.py
@@ -1,24 +1,10 @@
-import csv
+import db
 import random
 
-def read_money_file():
-    with open("money.csv", newline="") as file:
-        reader= csv.reader(file)
-        for row in reader:
-            current_money=float(row[1])
-
-    return current_money
-
-def write_money_file(current_money):
-    with open("money.csv", "w", newline="") as file:
-        writer=csv.writer(file)
-        new_balance= round(current_money, 2)
-        writer.writerow(["Money", new_balance])
-
+#imported the read and write txt functions on db.py
 #prototype idea of dealer's show card for the time being
 #switching the suit, rank and point_value arg to main
-def get_deck_list(suit, rank, point_value):
-
+def create_deck_list(suit, rank, point_value):
     """suit_choice=random.choice(suit)
     rank_choice=random.choice(rank)
     print(f"{rank_choice} of {suit_choice}")"""
@@ -28,21 +14,19 @@ def get_deck_list(suit, rank, point_value):
             point=point_value[i]
             card=[suits, ranks, point]
             deck.append(card)
-        random.shuffle(deck)
-
-
-    print(deck)
-
-
+    return deck
 
 
 
 
 def main():
+    print("BLACKJACK")
+    print("Blackjack payout is 3:2")
     suit= ["Hearts", "Diamonds", "Clubs", "Spades"]
     rank=["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     point_value=["2", "3", "4", "5", "6", "7", "8", "9", "10","10","10", "10", "11"]
-    get_deck_list(suit,rank,point_value)
+    create_deck_list(suit,rank,point_value)
+    print(create_deck_list(suit, rank, point_value))
 
 if __name__=="__main__":
     main()
---
+++ db.py
@@ -0,0 +1,19 @@
+
+FILENAME= "money.txt"
+
+def read_money():
+    with open(FILENAME, "r") as infile:
+        money=infile.readlines()
+        return float(money[0])
+
+def write_money(money_amount):
+    with open(FILENAME, "w", newline="") as outfile:
+        outfile.write(str(money_amount))
+
+def main():
+    money_amount = read_money()
+    print(money_amount)
+    write_money(money_amount)
+
+if __name__=="__main__":
+    main()
